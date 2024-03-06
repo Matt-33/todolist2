22fb8c353895add5b16b3b1520ef93ef7cd2d4f3
Matt-33 <matthias.giraudeau@gmail.com>
2024-03-06 15:22:22 +0100

    modif

--- js/components/editTodo.js
+++ js/components/editTodo.js
@@ -13,25 +13,20 @@ function editTodo(todoToEdit) {
   dueDateInput.value = todoToEdit.dueDate;
   priorityInput.value = todoToEdit.priority;
 
-  // Modification du texte du bouton "Ajouter" en "Enregistrer" pour indiquer l'édition
   const submitButton = document.querySelector('button[type="submit"]');
   submitButton.textContent = "Enregistrer";
 
-  // Modification du gestionnaire d'événement du formulaire pour prendre en charge l'édition
   document.getElementById('todoForm').removeEventListener('submit', addTodo);
   document.getElementById('todoForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    // Mise à jour des détails de la todo dans le tableau des todos
     todoToEdit.title = titleInput.value;
     todoToEdit.description = descriptionInput.value;
     todoToEdit.dueDate = dueDateInput.value;
     todoToEdit.priority = priorityInput.value;
 
-    // Ré-affichage de la liste de todos après l'édition
     renderTodoList();
 
-    // Réinitialisation du formulaire et restauration du bouton "Ajouter"
     submitButton.textContent = "Ajouter";
     document.getElementById('todoForm').removeEventListener('submit', editTodo);
     document.getElementById('todoForm').addEventListener('submit', addTodo);
